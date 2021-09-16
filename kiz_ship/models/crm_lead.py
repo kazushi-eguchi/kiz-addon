1ba3587ba7c01f81e00d2c8a12cbcb656030be96
江口和志 <sandwhale1010@gmail.com>
2021-09-16 16:13:03 +0900

    CRM連動

--- kiz_ship/models/crm_lead.py
+++ kiz_ship/models/crm_lead.py
@@ -2,9 +2,10 @@
 
 from odoo import _, api, fields, models
 
+
 class CrmLead(models.Model):
     _inherit = "crm.lead"
     ship_id = fields.Many2one(
-        comodel_name="ships.ship",
+        "ships.ship",
         string="ship"
     )
--- kiz_ship/models/ships.py
+++ kiz_ship/models/ships.py
@@ -10,6 +10,8 @@ class ShipsShip(models.Model):
     sno = fields.Char("sno", required=True)
     note = fields.Text(string="Description")
     ship_image = fields.Binary(string="Ship Image")
-    # lead_id = fields.One2many(
-    #     comodel_name="crm.lead",
-    # )
+    ship_ids = fields.One2many(
+          comodel_name="crm.lead",
+          inverse_name="ship_id",
+          string="lead",
+    )
