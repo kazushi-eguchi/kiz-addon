2cc3057743a5837ccc10e8499b251f88eb6d0c8b
kazushi eguchi <sandwhale1010@gmail.com>
2021-10-20 15:23:01 +0900

    webアイコン追加

--- kiz_purchase_ctm/__manifest__.py
+++ kiz_purchase_ctm/__manifest__.py
@@ -26,6 +26,7 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
+        'report/purchase_order_report.xml',
         # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
